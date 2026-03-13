926fd37108b82ce505f98ca06bb2e378cad0c27e
Dharur Mythri <dharurmythri@gmail.com>
2026-03-13 14:42:40 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,16 +3,16 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # Load model
-model = load_model("model/lstm_model.h5")
+model = load_model("lstm_model.h5")
 
 # Load tokenizer
-with open("model/tokenizer.pkl","rb") as f:
+with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
 
 def predict_spam(message):
 
     seq = tokenizer.texts_to_sequences([message])
-    padded = pad_sequences(seq,maxlen=100)
+    padded = pad_sequences(seq, maxlen=100)
 
     prediction = model.predict(padded)[0][0]
 
